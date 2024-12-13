3942250776cbcd772e6670cc05af5ed768f51209
Reflicate3 <144221916+Reflicate3@users.noreply.github.com>
2024-12-13 10:59:41 +0300

    MainActivity.kt

--- src/main/java/com/example/ningthprackotlin/MainActivity.kt
+++ src/main/java/com/example/ningthprackotlin/MainActivity.kt
@@ -1,40 +1,84 @@
-package com.example.ningthprackotlin
+package ru.mirea.prac9
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.ningthprackotlin.ui.theme.NingthPracKotlinTheme
+import androidx.compose.ui.unit.dp
+import ru.mirea.prac9.ui.theme.AppTheme
+import ru.mirea.prac9.ui.theme.AppTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NingthPracKotlinTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    StudentInfo(
-                        fullName = "Савин Андрей Сергеевич",
-                        groupNumber = "Группа ИКБО-07-22",
-                        modifier = Modifier.padding(innerPadding)
+            Greeting()
+        }
+    }
+}
+
+@Preview(
+    //uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    showSystemUi = true
+    )
+@Composable
+fun Greeting() {
+    AppTheme {
+        Scaffold(
+            modifier =  Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.surface)
+        ) { innerPadding ->
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .padding(innerPadding)
+                    .background(
+                        color = MaterialTheme.colorScheme.surfaceContainer,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+            ) {
+                //Spacer(modifier = Modifier.height(100.dp))
+                Box(
+
+                    modifier = Modifier
+                        .padding(top = 100.dp, bottom = 10.dp)
+                        .background(
+                            color = MaterialTheme.colorScheme.secondaryContainer,
+                            shape = CircleShape
+                        )
+                ){
+                    Text(
+                        text = "Емелин Владимир Владимирович ИКБО-12-22",
+                        color = MaterialTheme.colorScheme.primary,
+                        style = AppTypography.headlineLarge,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.padding(6.dp)
                     )
                 }
             }
         }
     }
-}
-
-@Composable
-fun StudentInfo(fullName: String, groupNumber: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "$fullName\n$groupNumber",
-        modifier = modifier
-    )
 }
